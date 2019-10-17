e9d6ed52251287e239f036254c28e9bda253ad48
Harry Le <harryle.au@gmail.com>
2019-10-17 17:16:19 +1100

    fix analytics

--- src/firestore/db.js
+++ src/firestore/db.js
@@ -1,4 +1,7 @@
-import firebase from 'firebase'
+// import firebase from 'firebase'
+import * as firebase from 'firebase/app'
+import 'firebase/analytics'
+import 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCBEA1NXLkIjO5SWRgVa5NljKylb9eBteY',
@@ -7,6 +10,8 @@ const firebaseConfig = {
   projectId: 'firechat-harryle',
   storageBucket: 'firechat-harryle.appspot.com',
   messagingSenderId: '48607968897',
-  appId: '1:48607968897:web:ba48789522d7da7b4f9780'
+  appId: '1:48607968897:web:ba48789522d7da7b4f9780',
+  measurementId: 'G-SW2YT2FBFD'
 }
 export const db = firebase.initializeApp(firebaseConfig).firestore()
+firebase.analytics()
